16a24e1a64066e230222bb2fe43273fb53bdc200
Boburmirzo Muhibulalev <bmuhibullaev@maiul.ru>
2022-11-07 18:47:46 +0900

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
-
+import '../styles/globals.css';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { useState } from 'react';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />;
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
